8fc992972611f5b2344c3653e95da59a4f2b6231
johsnsmartinez <johan.martinez.dev@gmail.com>
2022-08-06 11:56:16 -0500

    arreglando index

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ import cors from "cors";
 const app=express();
 const PORT= process.env.PORT || 3002;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.use(cors());
 app.use(express.json());
 
