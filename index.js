3cab41b63e181eeef76e8c9007f976f89e75aaa0
johsnsmartinez <johan.martinez.dev@gmail.com>
2022-08-06 12:07:14 -0500

    arreglando cors

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ import cors from "cors";
 const app=express();
 const PORT= process.env.PORT || 3002;
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+const config = {
+    application: {
+        cors: {
+            server: [
+                {
+                    origin: "*", //servidor que deseas que consuma o (*) en caso que sea acceso libre
+                    credentials: true
+                }
+            ]
+        }
+    }
+}
 
-app.use(cors());
+app.use(cors(
+    config.application.cors.server
+));
+
 app.use(express.json());
 
 import eps from './routes/eps';
